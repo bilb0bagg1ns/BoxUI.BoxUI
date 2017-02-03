0be156b52165f339fe15fd68aacdcf45346ab402
bilb0bagg1ns <mprasad@regis.edu>
2017-02-03 09:15:49 -0700

    Null checks added when Gradebook is being displayed.

--- boxui/src/main/java/com/box/model/domain/Grade.java
+++ boxui/src/main/java/com/box/model/domain/Grade.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 /**
  * Box UI user.
@@ -14,84 +16,71 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "grade")
 public class Grade {
 
-    @Id
-    public String id;
+	@Id
+	public String id;
 
-    /** associated user */
-    private String userId;
-    /** lesson associated with grade */
-    private String lessonId;
-    /** associated skill level type id (Novice, Intermediate, Expert) */
-    private String skillLevelTypeId;
-    /** pass or fail */
+	/** associated user */
+	private String userId;
+	/** lesson associated with grade */
+	private String lessonId;
+	/** associated skill level type id (Novice, Intermediate, Expert) */
+	private String skillLevelTypeId;
+	/** pass or fail */
 	private String grade;
 	/** date challenge taken/grade assigned */
+	@DateTimeFormat(iso = ISO.DATE_TIME)
 	private Date gradeDate;
-	
-	
+
 	public Grade() {
 	}
-
 
 	public String getId() {
 		return id;
 	}
 
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 
 	public String getUserId() {
 		return userId;
 	}
 
-
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-
 
 	public String getLessonId() {
 		return lessonId;
 	}
 
-
 	public void setLessonId(String lessonId) {
 		this.lessonId = lessonId;
 	}
-
 
 	public String getGrade() {
 		return grade;
 	}
 
-
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
-
 
 	public String getSkillLevelTypeId() {
 		return skillLevelTypeId;
 	}
 
-
 	public void setSkillLevelTypeId(String skillLevelTypeId) {
 		this.skillLevelTypeId = skillLevelTypeId;
 	}
-
 
 	public Date getGradeDate() {
 		return gradeDate;
 	}
 
-
 	public void setGradeDate(Date gradeDate) {
 		this.gradeDate = gradeDate;
 	}
-
 
 	@Override
 	public String toString() {
@@ -99,7 +88,4 @@ public class Grade {
 				+ skillLevelTypeId + ", grade=" + grade + ", gradeDate=" + gradeDate + "]";
 	}
 
-
-
-		
 }
